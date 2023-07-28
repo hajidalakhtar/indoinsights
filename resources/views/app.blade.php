158f147071e403f34c56a7fd1f622172c96ccdd0
hajidalakhtar <hajidalakhtar@gmail.com>
2023-07-28 22:00:24 +0700

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,17 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+<link rel="manifest" href="site.webmanifest">
+
+<meta http-equiv="Content-Security-Policy" content="img-src * 'self' data: https: blob:;">
+	
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+              <meta name="dicoding:email" content="hajidalakhtar@gmail.com">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
